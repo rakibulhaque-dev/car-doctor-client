6bea2d50a5bfeaa571e89c63f1ed1622d674dcfb
rakibulhaqueonline <rakibulhaque.info@gmail.com>
2023-05-14 19:37:03 +0600

    jwt functionalised on Login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -19,8 +19,24 @@ const Login = () => {
         signIn(email, password)
             .then(result => {
                 const user = result.user;
-                navigate(from, { replace: true })
-                console.log(user)
+                // navigate(from, { replace: true })
+                const loggedUser = {
+                    email: user.email
+                }
+                console.log(loggedUser)
+
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(loggedUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                    })
+
             })
             .catch(error => console.log(error))
     }
